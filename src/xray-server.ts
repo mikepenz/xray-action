263b98cf5ecb5e1eb21ea2ff189e5391bbeab59a
René Adam <98546423+radamlhind@users.noreply.github.com>
2022-12-06 16:49:42 +0100

    Adjust file

--- src/xray-server.ts
+++ src/xray-server.ts
@@ -4,176 +4,176 @@ import * as core from '@actions/core'
 import FormData from 'form-data'
 import {doFormDataRequest} from './utils'
 import {
-    createSearchParams,
-    retrieveFileExtension,
-    updateTestExecJson,
-    updateTestJson
+  createSearchParams,
+  retrieveFileExtension,
+  updateTestExecJson,
+  updateTestJson
 } from './xray-utils'
 import {Xray} from './xray'
 
 export class XrayServer implements Xray {
-    xrayBaseUrl: URL
-    searchParams!: URLSearchParams
-    token = ''
+  xrayBaseUrl: URL
+  searchParams!: URLSearchParams
+  token = ''
 
-    // XrayServer does not require authentication (uses BasicAuth or Token)
-    requiresAuth = false
+  // XrayServer does not require authentication (uses BasicAuth or Token)
+  requiresAuth = false
 
-    constructor(
-        private xrayOptions: XrayOptions,
-        private xrayImportOptions: XrayImportOptions
+  constructor(
+    private xrayOptions: XrayOptions,
+    private xrayImportOptions: XrayImportOptions
+  ) {
+    this.xrayBaseUrl =
+      this.xrayOptions.baseUrl || new URL('https://sandbox.xpand-it.com')
+    this.searchParams = createSearchParams(this.xrayImportOptions)
+  }
+
+  private protocol(): 'https:' | 'http:' {
+    if (this.xrayBaseUrl.protocol === 'http') {
+      return 'http:'
+    } else {
+      return 'https:'
+    }
+  }
+
+  async auth(): Promise<void> {
+    // no auth needed
+  }
+
+  updateTestExecKey(testExecKey: string): void {
+    this.xrayImportOptions.testExecKey = testExecKey
+    this.searchParams = createSearchParams(this.xrayImportOptions)
+  }
+
+  async import(data: Buffer, mimeType: string): Promise<string> {
+    // do import
+    let authString = ''
+    if (this.xrayOptions.token) {
+      authString = `Bearer ${this.xrayOptions.token}`
+    } else {
+      authString = `Basic ${Buffer.from(
+        `${this.xrayOptions.username}:${this.xrayOptions.password}`
+      ).toString('base64')}`
+    }
+
+    if (
+      this.xrayImportOptions.testExecutionJson &&
+      this.xrayImportOptions.testExecKey === ''
     ) {
-        this.xrayBaseUrl =
-            this.xrayOptions.baseUrl || new URL('https://sandbox.xpand-it.com')
-        this.searchParams = createSearchParams(this.xrayImportOptions)
-    }
-
-    private protocol(): 'https:' | 'http:' {
-        if (this.xrayBaseUrl.protocol === 'http') {
-            return 'http:'
-        } else {
-            return 'https:'
+      const form = new FormData()
+      updateTestExecJson(
+        this.xrayImportOptions,
+        this.xrayImportOptions.testExecutionJson
+      )
+      form.append(
+        'info',
+        JSON.stringify(this.xrayImportOptions.testExecutionJson),
+        {
+          contentType: 'application/json',
+          filename: 'info.json',
+          filepath: 'info.json'
         }
-    }
+      )
 
-    async auth(): Promise<void> {
-        // no auth needed
-    }
+      const apiPartName = 'file'
 
-    updateTestExecKey(testExecKey: string): void {
-        this.xrayImportOptions.testExecKey = testExecKey
-        this.searchParams = createSearchParams(this.xrayImportOptions)
-    }
+      const fileExtension = retrieveFileExtension(mimeType)
+      form.append(apiPartName, data.toString('utf-8'), {
+        contentType: mimeType,
+        filename: `report.${fileExtension}`,
+        filepath: `report.${fileExtension}`
+      })
 
-    async import(data: Buffer, mimeType: string): Promise<string> {
-        // do import
-        let authString = ''
-        if (this.xrayOptions.token) {
-            authString = `Bearer ${this.xrayOptions.token}`
-        } else {
-            authString = `Basic ${Buffer.from(
-                `${this.xrayOptions.username}:${this.xrayOptions.password}`
-            ).toString('base64')}`
+      updateTestJson(this.xrayImportOptions, this.xrayImportOptions.testJson)
+      if (this.xrayImportOptions.testJson) {
+        form.append(
+          'testInfo',
+          JSON.stringify(this.xrayImportOptions.testJson),
+          {
+            contentType: 'application/json',
+            filename: 'testInfo.json',
+            filepath: 'testInfo.json'
+          }
+        )
+      }
+
+      core.debug(
+        `Using multipart endpoint: ${this.xrayBaseUrl.href}/rest/raven/2.0/import/execution/multipart`
+      )
+
+      const importResponse = await doFormDataRequest(form, {
+        protocol: this.protocol(),
+        host: this.xrayBaseUrl.host,
+        headers: {
+          Authorization: authString
+        },
+        path: `${this.xrayBaseUrl.pathname}/rest/raven/2.0/import/execution/multipart`
+      })
+      try {
+        return importResponse.testExecIssue.key
+      } catch (error) {
+        core.warning(
+          `🔥 Response did not match expected format: ${JSON.stringify(
+            importResponse
+          )}`
+        )
+        return ''
+      }
+    } else {
+      if (mimeType === 'application/xml') {
+        const form = new FormData()
+        form.append('file', data.toString('utf-8'), {
+          contentType: mimeType,
+          filename: 'report.xml',
+          filepath: 'report.xml'
+        })
+
+        const importResponse = await doFormDataRequest(form, {
+          protocol: this.protocol(),
+          host: this.xrayBaseUrl.host,
+          headers: {
+            Authorization: authString
+          },
+          path: `${
+            this.xrayBaseUrl.pathname
+          }/rest/raven/2.0/import/execution?${this.searchParams.toString()}`
+        })
+        try {
+          return importResponse.testExecIssue.key
+        } catch (error) {
+          core.warning(
+            `🔥 Response did not match expected format: ${JSON.stringify(
+              importResponse
+            )}`
+          )
+          return ''
         }
+      } else {
+        const endpoint = `${this.xrayBaseUrl.href}/rest/raven/2.0/import/execution`
+        core.debug(`Using endpoint: ${endpoint}`)
 
-        if (
-            this.xrayImportOptions.testExecutionJson &&
-            this.xrayImportOptions.testExecKey === ''
-        ) {
-            const form = new FormData()
-            updateTestExecJson(
-                this.xrayImportOptions,
-                this.xrayImportOptions.testExecutionJson
-            )
-            form.append(
-                'info',
-                JSON.stringify(this.xrayImportOptions.testExecutionJson),
-                {
-                    contentType: 'application/json',
-                    filename: 'info.json',
-                    filepath: 'info.json'
-                }
-            )
-
-            const apiPartName = 'file'
-
-            const fileExtension = retrieveFileExtension(mimeType)
-            form.append(apiPartName, data.toString('utf-8'), {
-                contentType: mimeType,
-                filename: `report.${fileExtension}`,
-                filepath: `report.${fileExtension}`
-            })
-
-            updateTestJson(this.xrayImportOptions, this.xrayImportOptions.testJson)
-            if (this.xrayImportOptions.testJson) {
-                form.append(
-                    'testInfo',
-                    JSON.stringify(this.xrayImportOptions.testJson),
-                    {
-                        contentType: 'application/json',
-                        filename: 'testInfo.json',
-                        filepath: 'testInfo.json'
-                    }
-                )
-            }
-
-            core.debug(
-                `Using multipart endpoint: ${this.xrayBaseUrl.href}/rest/raven/2.0/import/execution/multipart`
-            )
-
-            const importResponse = await doFormDataRequest(form, {
-                protocol: this.protocol(),
-                host: this.xrayBaseUrl.host,
-                headers: {
-                    Authorization: authString
-                },
-                path: `${this.xrayBaseUrl.pathname}/rest/raven/2.0/import/execution/multipart`
-            })
-            try {
-                return importResponse.testExecIssue.key
-            } catch (error) {
-                core.warning(
-                    `🔥 Response did not match expected format: ${JSON.stringify(
-                        importResponse
-                    )}`
-                )
-                return ''
-            }
-        } else {
-            if (mimeType === 'application/xml') {
-                const form = new FormData()
-                form.append('file', data.toString('utf-8'), {
-                    contentType: mimeType,
-                    filename: 'report.xml',
-                    filepath: 'report.xml'
-                })
-
-                const importResponse = await doFormDataRequest(form, {
-                    protocol: this.protocol(),
-                    host: this.xrayBaseUrl.host,
-                    headers: {
-                        Authorization: authString
-                    },
-                    path: `${
-                        this.xrayBaseUrl.pathname
-                    }/rest/raven/2.0/import/execution?${this.searchParams.toString()}`
-                })
-                try {
-                    return importResponse.testExecIssue.key
-                } catch (error) {
-                    core.warning(
-                        `🔥 Response did not match expected format: ${JSON.stringify(
-                            importResponse
-                        )}`
-                    )
-                    return ''
-                }
-            } else {
-                const endpoint = `${this.xrayBaseUrl.href}/rest/raven/2.0/import/execution`
-                core.debug(`Using endpoint: ${endpoint}`)
-
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                const importResponse = await got.post<any>(endpoint, {
-                    searchParams: this.searchParams,
-                    headers: {
-                        Authorization: authString,
-                        'Content-Type': mimeType
-                    },
-                    body: data,
-                    responseType: 'json',
-                    timeout: 60000 // 60s timeout
-                })
-                try {
-                    return importResponse.body.testExecIssue.key
-                } catch (error) {
-                    core.warning(
-                        `🔥 Response did not match expected format: ${JSON.stringify(
-                            importResponse.body || importResponse
-                        )}`
-                    )
-                    return ''
-                }
-            }
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const importResponse = await got.post<any>(endpoint, {
+          searchParams: this.searchParams,
+          headers: {
+            Authorization: authString,
+            'Content-Type': mimeType
+          },
+          body: data,
+          responseType: 'json',
+          timeout: 60000 // 60s timeout
+        })
+        try {
+          return importResponse.body.testExecIssue.key
+        } catch (error) {
+          core.warning(
+            `🔥 Response did not match expected format: ${JSON.stringify(
+              importResponse.body || importResponse
+            )}`
+          )
+          return ''
         }
+      }
     }
+  }
 }
